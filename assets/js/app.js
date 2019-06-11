ef36d7b723127a551e5fec29506045376af0f2d9
Tim Mecklem <timothy@mecklem.com>
2019-06-10 22:28:39 -0400

    Fix javascript warnings

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
 import '../css/app.scss'
 import 'phoenix_html'
 import './live-view.js'
+
+library.add(fas, far, fab)
