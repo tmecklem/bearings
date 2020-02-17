fa1e893e7fbb757771d610abaae493d3a8b726bc
Tim Mecklem <timothy@mecklem.com>
2020-02-17 08:26:55 -0500

    Use config host if set

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :bearings, BearingsWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "bearings.herokuapp.com", port: 443],
+  url: [scheme: "https", host: System.get_env("APP_HOST") || "bearings.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
