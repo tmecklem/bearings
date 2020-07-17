1d658458a45521fcae9d3bfbde026b40f896c207
Tim Mecklem <timothy@mecklem.com>
2020-07-17 17:05:35 -0400

    Update usage of liveview

--- lib/bearings_web/router.ex
+++ lib/bearings_web/router.ex
@@ -5,7 +5,8 @@ defmodule BearingsWeb.Router do
   pipeline :browser do
     plug(:accepts, ["html"])
     plug(:fetch_session)
-    plug :fetch_live_flash
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, {BearingsWeb.LayoutView, :root})
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
     plug(BearingsWeb.Auth, repo: Bearings.Repo)
