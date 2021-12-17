902104b93eb8464d13f065e0002ca7467856cd67
Tim Mecklem <timothy@mecklem.com>
2021-12-16 23:35:57 -0500

    mix format

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,4 @@ import Config
 
 config :logger, level: :info
 
-config(:bearings, BearingsWeb.Endpoint,
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
-)
+config(:bearings, BearingsWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]])
